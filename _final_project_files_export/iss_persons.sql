ef2f07fecb2be1cb375334d20079517027a1e055
KingDylan1231 <65315463+KingDylan1231@users.noreply.github.com>
2025-04-09 11:08:46 -0400

    Add files via upload

---
+++ _final_project_files_export/iss_persons.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 09, 2025 at 04:46 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cis355`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `iss_persons`
+--
+
+CREATE TABLE `iss_persons` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `mobile` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `pwd_hash` varchar(255) NOT NULL,
+  `pwd_salt` varchar(255) NOT NULL,
+  `admin` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `iss_persons`
+--
+
+INSERT INTO `iss_persons` (`id`, `fname`, `lname`, `mobile`, `email`, `pwd_hash`, `pwd_salt`, `admin`) VALUES
+(2, 'apple', 'appleee', '000-000-0000', 'apple@apple.com', '5eb07a0c9cf76dc1d21fd0c8561116fe', '40fc59dc0706e1faea0fec1619d82789', 'No'),
+(3, 'Dylan', 'Wiza', '000-000-0001', 'wizadylan@gmail.com', '73ab5fc818cb24f15ed5e9ec7f3d3794', 'e94985cb2cd77a48ca0660c076b96159', 'y');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `iss_persons`
+--
+ALTER TABLE `iss_persons`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `iss_persons`
+--
+ALTER TABLE `iss_persons`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
